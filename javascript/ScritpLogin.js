408246d8b4adccc0fefd09677b63345b9b369623
diogenes-omar-lora <omarlora08@hotmail.com>
2023-05-03 22:00:17 -0400

    login done

---
+++ javascript/ScritpLogin.js
@@ -0,0 +1,14 @@
+let x = document.getElementById("login");
+let y = document.getElementById("signup");
+let z = document.getElementById("elegir");
+
+function login(){
+    x.style.left = "0";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
+function signup(){
+    x.style.left = "-450px";
+    y.style.left = "0";
+    z.style.left = "120px";
+}
