5c2021cede5e090e63d767b9e12c43e0d42c40f4
diogenes-omar-lora <omarlora08@hotmail.com>
2023-05-13 01:33:41 -0400

    listo

--- javascript/ScritpLogin.js
+++ javascript/ScritpLogin.js
@@ -12,3 +12,32 @@ function signup(){
     y.style.left = "0";
     z.style.left = "120px";
 }
+
+//scroll
+const carrousel= document.querySelector(".carrousel");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) =>{
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carrousel.scrollLeft;
+
+}
+const dragging = (e) =>{
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carrousel.scrollLeft = prevScrollLeft - positionDiff;
+
+}
+const dragStop = ()=>{
+    isDragStart = false;
+}
+carrousel.addEventListener( "mousedown" ,dragStart);
+carrousel.addEventListener( "touchstart" ,dragStart);
+
+carrousel.addEventListener( "mousemove" ,dragging) ;
+carrousel.addEventListener( "touchmove" ,dragging) ;
+
+carrousel.addEventListener( "mouseup" ,dragStop) ;
